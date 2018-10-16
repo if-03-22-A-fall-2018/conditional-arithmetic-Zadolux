4d365cfb596c74ea7c2fc7f054045412dc11f64e
Wallinger Marc <zadolux@inscomers.net>
2018-10-16 10:25:54 +0200

    Initial commit

---
+++ condarith.c
@@ -0,0 +1,16 @@
+/*----------------------------------------------------------
+ *				HTBLA-Leonding / Class: 2AHIF
+ * ---------------------------------------------------------
+ * Exercise Number: 3
+ * Title:			Conditional arithmetic
+ * Author:	  Wallinger Marc
+ * ----------------------------------------------------------
+ * Description:
+ * Exercises for pointers (call by ref, val, ...)
+ * ----------------------------------------------------------
+*/
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+  return 0;
+}
