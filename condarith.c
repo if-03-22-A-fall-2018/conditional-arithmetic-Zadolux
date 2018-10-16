42e094539825b532cf7811b71e4af47cc9951c32
Wallinger Marc <zadolux@inscomers.net>
2018-10-16 11:05:42 +0200

    PC problems kinda fixed

--- condarith.c
+++ condarith.c
@@ -11,6 +11,22 @@
 */
 #include <stdio.h>
 
+void showMenu()
+{
+  printf("Choose one of the following operations:\n");
+  printf("\tAdd (1)\n");
+  printf("\tMultiply (2)\n");
+}
+
+// Reads the two needed numbers
+void readNumbers(double* number1, double* number2)
+{
+  printf("Number 1: ");
+  scanf("%lf", number1);
+  printf("Number 2: ");
+  scanf("%lf", number2);
+}
+
 int main(int argc, char const *argv[]) {
-  return 0;
+  showMenu();
 }
